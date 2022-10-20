48d02b82a59c66c565b11c345b9d5d36d29b9221
Fell <106463662+Fellshadow@users.noreply.github.com>
2022-10-20 19:06:11 +0100

    Update Ironmon-Tracker.lua

--- Ironmon-Tracker.lua
+++ Ironmon-Tracker.lua
@@ -1,4 +1,5 @@
-Main = {} -- luacheck: globals Main
+-- luacheck: globals Main
+Main = {}
 
 -- The latest version of the tracker. Should be updated with each PR.
 Main.Version = { major = "6", minor = "4", patch = "2" }
